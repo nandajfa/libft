889e763aa9b8250bbc634fa2987b83ea4bfda2d8
nandajfa <nanda.jfa@gmail.com>
2021-10-02 12:58:22 -0300

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jefernan <jefernan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 23:11:27 by jefernan          #+#    #+#             */
-/*   Updated: 2021/10/01 22:05:33 by jefernan         ###   ########.fr       */
+/*   Updated: 2021/10/02 12:57:00 by jefernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ static int	ft_count(int n)
 	i = 0;
 	if (n == 0)
 		i = 1;
+	if (n < 0)
+	{
+		i++;
+		n = -1 * n;
+	}
 	while (n != 0)
 	{
 		n /= 10;
@@ -29,32 +34,29 @@ static int	ft_count(int n)
 
 char	*ft_itoa(int n)
 {
-	int		i;
+	long	num;
 	int		count;
 	char	*str;
 
 	count = ft_count(n);
-	i = count;
-	if (n == -2147483648)
+	num = (long)n;
+	if (num == -2147483648)
 		return (ft_strdup("-2147483648"));
-	if (n < 0)
-	{
-		str = (char *)ft_calloc((count + 2), sizeof(char));
-		str[0] = '-';
-		n *= -1;
-		i++;
-	}
-	else
-		str = (char *)ft_calloc((count + 1), sizeof(char));
+	str = (char *)ft_calloc((count + 1), sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	if (n == 0)
+	if (num == 0)
 		str[0] = '0';
-	while (n != 0)
+	if (num < 0)
 	{
-		str[--i] = n % 10 + '0';
-		n /= 10;
+		str[0] = '-';
+		num *= -1;
+	}
+	count--;
+	while (num != 0 && count >= 0)
+	{
+		str[count--] = num % 10 + '0';
+		num /= 10;
 	}
 	return (str);
 }
-
